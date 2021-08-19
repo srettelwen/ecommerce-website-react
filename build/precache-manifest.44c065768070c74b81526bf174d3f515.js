f00bc62148799b3f6fcee727d9fa8f7954b16532
srettelwen <srettelwenbhushan@gmail.com>
2021-08-19 23:29:39 +0530

    first commit

---
+++ build/precache-manifest.44c065768070c74b81526bf174d3f515.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bba7567ed76ca2b0df17",
+    "url": "/static/css/main.d54f9de8.chunk.css"
+  },
+  {
+    "revision": "bba7567ed76ca2b0df17",
+    "url": "/static/js/main.6e79ab7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c44af2f97180d850d7d6",
+    "url": "/static/js/2.bc4e413d.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "3b8320b64547c6859a092449410b7766",
+    "url": "/index.html"
+  }
+];
